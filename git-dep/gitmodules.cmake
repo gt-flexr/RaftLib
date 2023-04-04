17c4627672aaa698a43ec0fe2447996b8285da53
Jin Heo <993jin@gmail.com>
2023-04-04 17:03:30 -0400

    update submodules

--- git-dep/gitmodules.cmake
+++ git-dep/gitmodules.cmake
@@ -1,9 +1,9 @@
 ##
 # SET SPECIFIC NEEDED OPTIONS
 # - will be ignored by platforms that don't include these options
-# so not a huge deal. 
+# so not a huge deal.
 ##
-set( DEMANGLE_CUSTOM_NAMESPACE TRUE ) 
+set( DEMANGLE_CUSTOM_NAMESPACE TRUE )
 set( DEMANGLE_NAMESPACE "raft" )
 
 set( AFFINITY_CUSTOM_NAMESPACE TRUE )
@@ -13,17 +13,17 @@ set( AFFINITY_NAMESPACE "raft" )
 # list git repo dependencies here
 ##
 if( WIN32 )
-set( GIT_MODULES 
-        cmdargs 
-        demangle 
-        affinity 
+set( GIT_MODULES
+        cmdargs
+        demangle
+        affinity
         )
 else()
 ##
-set( GIT_MODULES 
-        shm 
-        cmdargs 
-        demangle 
-        affinity ) 
+set( GIT_MODULES
+        shm
+        cmdargs
+        demangle
+        affinity )
 endif()
 
